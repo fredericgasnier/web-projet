f0167ff38fc0174c62db45ae68a5ce900c0a9fbf
fredericgasnier <fredo95100@gmail.com>
2019-03-04 18:53:39 +0100

    modif 4 ListeServler.java

--- src/main/java/lan/fred/ListeServler.java
+++ src/main/java/lan/fred/ListeServler.java
@@ -20,13 +20,13 @@ public class ListeServler extends HttpServlet {
         protected void fredo(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
                 String yourName = (String) request.getParameter("nom");
                 if (yourName.equals("fred")) {
-                PrintWriter writer = response.getWriter();
-                writer.println("<h1>Hello " + yourName + "</h1>");
-                writer.println("<p> Salut " + yourName + ", ça va ?</p>");
-                writer.println("<p>Comment vas tu ?</p>");
-                writer.close();
+                //PrintWriter writer = response.getWriter();
+                out.println("<h1>Hello " + yourName + "</h1>");
+                out.println("<p> Salut " + yourName + ", ça va ?</p>");
+                out.println("<p>Comment vas tu ?</p>");
+                //writer.close();
                 } else {
-                writer.println("<p>Comment vas tu " + yourName + "?</p>");
+                out.println("<p>Comment vas tu " + yourName + "?</p>");
                 }
         }
 
