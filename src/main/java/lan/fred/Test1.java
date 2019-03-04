154cefe135740e7bbe8f801f635a78f1d0b4bdbf
fredericgasnier <fredo95100@gmail.com>
2019-03-04 12:09:56 +0100

    modif 3 Test1.java

--- src/main/java/lan/fred/Test1.java
+++ src/main/java/lan/fred/Test1.java
@@ -22,7 +22,7 @@ public class Test1 extends HttpServlet {
 
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        request.setAttribute("heure", "jour");
+        request.setAttribute("heure", "our");
         this.getServletContext().getRequestDispatcher("/bonjour1.jsp").forward(request, response);
     }
 
